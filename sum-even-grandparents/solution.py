71c894270a6a8339980139f6dadb7afbc28a3462
Moe <mohamed.amadou159@gmail.com>
2020-03-19 00:49:13 -0400

    Add day 13 p3 - solution

---
+++ sum-even-grandparents/solution.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # def getGrandchildren(self, gp):
+    #     if gp is None: return []
+    #     gc = []
+    #     if gp.left is not None:
+    #         gc.extend([gp.left.left, gp.left.right])
+    #     if gp.right is not None:
+    #         gc.extend([gp.right.left, gp.right.right])
+    #     gc = filter((lambda x: x is not None), gc)
+    #     return list(map((lambda x: x.val), gc))
+
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        """ Test cases:
+            I: root = [2,null,null]
+            O: 0
+
+            I: root = [2,4,6,null,null,null,null]
+            O: 0
+
+            I: root = [2,1,3,1,7,9,11]
+            O: 4
+        """
+        """ Optimized solution 1:
+            - Pre-order DFS using python stacks for flair
+        """
+        if not root: return 0  # empty tree
+
+        # initialization:
+        summation = 0
+
+        # root node, -1 for init, -1 for init
+        dfs_stack = [(root, -1, -1)]
+
+        # pre-order DFS traveral
+        while dfs_stack:
+            node, parent, grandparent = dfs_stack.pop()
+            if grandparent & 1 == 0: summation += node.val
+            if node.right: dfs_stack.append((node.right, node.val, parent))
+            if node.left: dfs_stack.append((node.left, node.val, parent))
+
+        return summation
+
+        """ Initial solution:
+            - One solution would be to work from the top-down, seeing whether or not the node is even and adding the grandchildrens' sums to our accumulated result (if the node is a grandparent)
+            - Define helper function to return list of grandchildren vals
+        """
+#         if root is None: return 0
+#         return self.sumEvenGrandparent(root.left) + self.sumEvenGrandparent(root.right) + (sum(self.getGrandchildren(root)) if root.val % 2 == 0 else 0)
+
+
